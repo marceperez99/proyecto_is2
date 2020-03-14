7c74411ebd5d0a7c894d828710cd95cabaa588f2
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-14 11:55:42 -0300

    Crea template base.html Modifica index.html para que este extienda del template base.html Modifica directorio base del proyecto en settings/base.py y agrega directorio de templates

--- proyecto_is2/settings/base.py
+++ proyecto_is2/settings/base.py
@@ -1,8 +1,7 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -35,7 +34,7 @@ ROOT_URLCONF = 'proyecto_is2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
