37b3de0e0775e743b7d216ffbb404b363cd2fbb8
Marcos Riveros <macros21b@fpuna.edu.py>
2020-03-15 19:18:52 -0300

    Documentacion del view del menu pricipal

--- sso/views.py
+++ sso/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+
 @login_required
 def index_view(request):
+    """Esta función se encarga de, una vez que el usuario haya iniciado sesión, redirigirla
+    al template que muestra el menú pricipal
+
+    Args:
+        request (HttpRequest Object): respuesta del request
+
+    Retorna:
+        El HttpResponse de la Vista a mostrarse
+
+    """
     contexto = {'user': request.user}
     return render(request, 'sso/index.html', context=contexto)
 # Create your views here.
