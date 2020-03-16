da3925703a344a03382ce1aa1cc6cea5d768aecf
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-16 19:50:29 -0300

    Agrega modelo proxy Usuario(User) y define un metodo de prueba.

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(User):
+    class meta:
+        proxy = True
+
+    #metodo de prueba
+    def introduccion(self):
+        return "Hola, mi nombre es {}".format(self.first_name)
