48231693dfcf2a53a8bf5505840eecc92fea9e6a
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-17 15:31:44 -0300

    Hace que las vistas de usuarios requieran que se haya inciciado sesión.

--- usuario/views.py
+++ usuario/views.py
@@ -3,13 +3,14 @@ from .models import Usuario
 
 
 # Create your views here.
-
+@login_required
 def usuarios_view(request):
     lista_usuario = list(Usuario.objects.all())
     contexto = {'lista_usuario': lista_usuario, 'user': request.user}
     return render(request, 'usuario/usuarios.html', context=contexto)
 
 
+@login_required
 def usuario_view(request, id_usuario):
     usuario = get_object_or_404(Usuario, pk=id_usuario)
     contexto = {'usuario': usuario, 'user': request.user}
