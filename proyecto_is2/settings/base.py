b21c5de69abc3c3dd754f16791bd744250c60a04
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-18 16:11:20 -0300

    Agrega reversion como Middleware y crea los revisions iniciales.

--- proyecto_is2/settings/base.py
+++ proyecto_is2/settings/base.py
@@ -30,6 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'reversion.middleware.RevisionMiddleware',
 ]
 
 ROOT_URLCONF = 'proyecto_is2.urls'
