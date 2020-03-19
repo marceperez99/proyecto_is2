9d4019579cce8ed4b7b7c021299e8758d091f51b
Luis Perez <luisperez.lpr91@fpuna.edu.py>
2020-03-19 04:27:43 -0300

    Agrega filtros, busquda y especifica los campos del modelo Proyecto que seran visibles en el panel de administracion

--- GestionDeProyecto/admin.py
+++ GestionDeProyecto/admin.py
@@ -5,4 +5,11 @@ from GestionDeProyecto.models import Proyecto
 # Register your models here.
 
 
-admin.site.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion", "creador", "fechaCreacion", "estado")
+    search_fields = ("creador", "fechaCreacion", "estado")
+    list_filter = ("fechaCreacion", "estado")
+    date_hierarchy = "fechaCreacion"
+
+
+admin.site.register(Proyecto, ProyectoAdmin)
