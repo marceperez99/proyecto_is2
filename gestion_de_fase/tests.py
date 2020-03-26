b9b146c6d7d3bb76744ff0a52263e82f7c91549d
Luis Perez <luisperez.lpr91@fpuna.edu.py>
2020-03-25 20:23:01 -0400

    Agrega prueba unitaria para comprobar la correcta insersion de una fase entre dos fases adyacentes

--- gestion_de_fase/tests.py
+++ gestion_de_fase/tests.py
@@ -1,3 +1,87 @@
-from django.test import TestCase
+from datetime import datetime
+import pytest
+from django.contrib.auth.models import User, Permission
+from gestion_de_fase.models import Fase
+from gestion_de_proyecto.models import Participante, Proyecto
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+from roles_de_proyecto.models import RolDeProyecto
+
+
+@pytest.fixture
+def usuario():
+    user = User(username='user_test', email='test@admin.com')
+    user.set_password('password123')
+    user.save()
+    return user
+
+
+@pytest.fixture
+def cliente_loggeado(usuario):
+    client = Client()
+    client.login(username='user_test', password='password123')
+    return client
+
+
+@pytest.fixture
+def rol_de_proyecto():
+    rol = RolDeProyecto(nombre='Desarrollador', descripcion='Descripcion del rol')
+    rol.save()
+    rol.asignar_permisos(list(Permission.objects.all().filter(codename__startswith='pp_')))
+    return rol
+
+
+@pytest.fixture
+def proyecto(usuario, rol_de_proyecto):
+    proyecto = Proyecto(nombre='Proyecto Prueba', descripcion='Descripcion de prueba', fecha_de_creacion=datetime.today(),
+                        creador=usuario)
+    proyecto.save()
+    participante = Participante.objects.create(proyecto=proyecto, usuario=usuario)
+    participante.save()
+    return proyecto
+
+
+@pytest.fixture()
+def fase(proyecto):
+    fase = Fase(nombre='Analisis', proyecto=proyecto, fase_cerrada=False, puede_cerrarse=False)
+    fase.save()
+    fase = Fase(nombre='Desarrollo', proyecto=proyecto, fase_cerrada=False, puede_cerrarse=False)
+    fase.save()
+    fase = Fase(nombre='Pruebas', proyecto=proyecto, fase_cerrada=False, puede_cerrarse=False)
+    fase.save()
+    return None
+
+
+
+@pytest.mark.django_db
+def test_nueva_fase_medio(proyecto):
+    """
+    Prueba unitaria para verificar que el metodo posicionar de una fase modifique correctamente el
+    atributo fase_anterior de las fases adyacentes de una nueva fase.
+    Se espera:
+        El aterior de la fase 1 se None
+        El de la fase 2 sea la fase 1
+        El de la fase 3 sea la fase 2
+
+    Mensaje de error:
+        No se logra posicionar una fase entre dos fases
+    """
+
+    fase_1 = Fase(nombre='Analisis', proyecto=proyecto, fase_cerrada=False, puede_cerrarse=False)
+    fase_1.fase_anterior = None
+    fase_1.posicionar_fase()
+    fase_1.save()
+    fase_3 = Fase(nombre='Pruebas', proyecto=proyecto, fase_cerrada=False, puede_cerrarse=False)
+    fase_3.fase_anterior = fase_1
+    fase_3.posicionar_fase()
+    fase_3.save()
+    fase_2 = Fase(nombre='Disenho', proyecto=proyecto, fase_cerrada=False, puede_cerrarse=False)
+    fase_2.fase_anterior = fase_1
+    fase_2.save()
+    fase_2.posicionar_fase()
+    fase_1 = Fase.objects.get(id=fase_1.id)
+    fase_2 = Fase.objects.get(id=fase_2.id)
+    fase_3 = Fase.objects.get(id=fase_3.id)
+    assert fase_1.fase_anterior is None and fase_2.fase_anterior.pk == fase_1.pk and fase_3.fase_anterior.pk == fase_2.pk, "No se logra posicionar una fase entre dos fases"
