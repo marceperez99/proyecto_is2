6cf6b0583edf1e54f0bcde1773b7b1a2a189ce85
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-27 17:39:53 -0400

    Agrega visualizacion de errores al crear el tipo de Item. Agrega visualizacion de lista de Tipos de Item
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_tipo_de_item/views.py
+++ gestion_de_tipo_de_item/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from gestion_de_proyecto.models import Proyecto
 from gestion_de_fase.models import Fase
 from gestion_de_tipo_de_item.forms import TipoDeItemForm, AtributoCadenaForm, AtributoArchivoForm, AtributoBooleanoForm, \
@@ -19,11 +19,13 @@ def tipo_de_item_view(request, proyecto_id, fase_id):
     Vista que permite visualizar los tipos de items dentro de una fase de un proyecto
 
     """
-
     proyecto = get_object_or_404(Proyecto, pk=proyecto_id)
     fase = get_object_or_404(proyecto.fase_set, pk=fase_id)
     lista_tipo_de_item = list(fase.tipodeitem_set.all())
-    contexto = {'user': request.user, 'lista_tipo_de_item': lista_tipo_de_item}
+    contexto = {'user': request.user,
+                'proyecto': proyecto,
+                'fase': fase,
+                'lista_tipo_de_item': lista_tipo_de_item}
     return render(request, 'gestion_de_tipo_de_item/tipos_de_items.html', context=contexto)
 
 
@@ -33,40 +35,43 @@ def nuevo_tipo_de_item_view(request, proyecto_id, fase_id):
     """
     proyecto = get_object_or_404(Proyecto, pk=proyecto_id)
     fase = get_object_or_404(proyecto.fase_set, pk=fase_id)
+    contexto = {'user': request.user,
+                'proyecto': proyecto,
+                'fase': fase,
+                'tipos_de_atributo_forms': {'cadena': AtributoCadenaForm(), 'archivo': AtributoArchivoForm(),
+                                            'booleano': AtributoBooleanoForm(), 'numerico': AtributoNumericoForm(),
+                                            'fecha': AtributoFechaForm()}
+                }
     if request.method == 'POST':
         tipo_de_item_form = TipoDeItemForm(request.POST)
         if tipo_de_item_form.is_valid():
             tipo_de_item = tipo_de_item_form.save(commit=False)
-            # tipo_de_item.
-
             # Lista de atributos dinamicos
             atributos_dinamicos = [dict() for x in range(int(request.POST['cantidad_atributos']))]
             # Se filtran todos los atributos dinamicos
             atributos_de_items = {key: request.POST[key] for key in request.POST if key.startswith("atr_")}
+            # se crea una lista con todos los atributos dinamicos
             for atributo in atributos_de_items.keys():
                 partes = atributo.split("_", maxsplit=2)
-                print(partes)
                 atributos_dinamicos[int(partes[1]) - 1][partes[2]] = atributos_de_items[atributo]
-            forms = []
-            print(atributos_dinamicos)
+            atributos_forms = []
             for atributo in atributos_dinamicos:
-                print(atributo)
                 if 'tipo' not in atributo.keys():
                     continue
                 if atributo['tipo'] == 'cadena':
-                    forms.append(AtributoCadenaForm(atributo))
+                    atributos_forms.append(AtributoCadenaForm(atributo))
                 elif atributo['tipo'] == 'numerico':
-                    forms.append(AtributoNumericoForm(atributo))
+                    atributos_forms.append(AtributoNumericoForm(atributo))
                 elif atributo['tipo'] == 'booleano':
-                    forms.append(AtributoBooleanoForm(atributo))
+                    atributos_forms.append(AtributoBooleanoForm(atributo))
                 elif atributo['tipo'] == 'fecha':
-                    forms.append(AtributoFechaForm(atributo))
+                    atributos_forms.append(AtributoFechaForm(atributo))
                 elif atributo['tipo'] == 'archivo':
-                    forms.append(AtributoArchivoForm(atributo))
+                    atributos_forms.append(AtributoArchivoForm(atributo))
             all_valid = True
-            for form in forms:
+            # Se validan todos los forms
+            for form in atributos_forms:
                 all_valid = all_valid and form.is_valid()
-            # if all( [ form.is_valid() for form in forms ] )
 
             if all_valid:
                 # TODO: poner cosas en tipo de item
@@ -74,26 +79,15 @@ def nuevo_tipo_de_item_view(request, proyecto_id, fase_id):
                 tipo_de_item.creador = request.user
                 tipo_de_item.fecha_creacion = timezone.now()
                 tipo_de_item.save()
-
-                for form in forms:
+                for form in atributos_forms:
                     atributo = form.save(commit=False)
                     atributo.tipo_de_item = tipo_de_item
                     atributo.save()
+                return redirect('index')
+            else:
+                contexto['form'] = tipo_de_item_form
+                contexto['atributos_seleccionados'] = atributos_forms
     else:
-        form = TipoDeItemForm()
-        form_cadena = AtributoCadenaForm()
-        form_archivo = AtributoArchivoForm()
-        form_booleano = AtributoBooleanoForm()
-        form_numerico = AtributoNumericoForm()
-        form_fecha = AtributoFechaForm()
-        contexto = {'user': request.user,
-                    'proyecto': proyecto,
-                    'fase': fase,
-                    'form': form,
-                    'form_archivo': form_archivo,
-                    'form_numerico': form_numerico,
-                    'form_booleano': form_booleano,
-                    'form_fecha': form_fecha,
-                    'form_cadena': form_cadena
-                    }
-        return render(request, 'gestion_de_tipo_de_item/nuevo_tipo_de_item.html', context=contexto)
+        contexto['form'] = TipoDeItemForm()
+
+    return render(request, 'gestion_de_tipo_de_item/nuevo_tipo_de_item.html', context=contexto)
