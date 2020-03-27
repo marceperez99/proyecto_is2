205c29fbcbde13c438027c0bb5662ce9a31c6266
Luis Perez <luisperez.lpr91@fpuna.edu.py>
2020-03-26 20:30:44 -0400

    Agrega documentacion a las vistas "nuevo_proyecto_view" y "visualizar_proyecto_view" de gestion_de_proyecto

--- gestion_de_proyecto/views.py
+++ gestion_de_proyecto/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-import datetime
 from django.utils import timezone
 from gestion_de_proyecto.forms import ProyectoForm
 from .models import Proyecto
@@ -7,6 +6,19 @@ from .models import Proyecto
 # Create your views here.
 
 def nuevo_proyecto_view(request):
+    """
+    Vista que se usa para la creacion de un proyecto
+    Si el metodo Http con el que se realizo la peticion fue GET se muestra un formulario para completar.
+    Si el metodo Http con el que se realizo la peticion fue POST se toman los datos recibidos y se guardan en la BD
+
+    Args:
+
+        request: HttpRequest
+
+    Retorna:
+
+        HttpResponse
+    """
     if request.method == 'POST':
         form = ProyectoForm(request.POST)
         if form.is_valid():
@@ -21,7 +33,20 @@ def nuevo_proyecto_view(request):
     return render(request, 'gestion_de_proyecto/nuevo_proyecto.html', {'formulario': form})
 
 
+
 def visualizar_proyecto_view(request, proyecto_id):
+    """
+    Vista que muestra al usuario toda la informacion de un proyecto.
+
+    Args:
+
+        request: HttpRequest
+        proyecto_id: int identificador unico del Proyecto que se quiere ver
+
+    Retorna:
+
+        HttpResponse
+    """
     proyecto = get_object_or_404(Proyecto, id=proyecto_id)
     contexto={'user': request.user, 'proyecto': proyecto}
     return render(request,'gestion_de_proyecto/visualizar_proyecto.html',contexto)
