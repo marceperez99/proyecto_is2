e76859a506db40fe5b8eedde1891f6e1c1d73dba
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-27 10:55:51 -0400

    Incluye las urls para ver participante.
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_proyecto/urls.py
+++ gestion_de_proyecto/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('nuevo/', views.nuevo_proyecto_view, name='nuevoProyecto'),
-    #path('visualizarProyecto/', views.visualizar_proyecto_view, name='visualizarProyecto'),
-    #path('', views.visualizar_proyectos_view, name='visualizarProyectos'),
-    #path('editar/', views.editar_proyecto_view, name='editarProyecto'),
+    # path('visualizarProyecto/', views.visualizar_proyecto_view, name='visualizarProyecto'),
+    # path('', views.visualizar_proyectos_view, name='visualizarProyectos'),
+    # path('editar/', views.editar_proyecto_view, name='editarProyecto'),
+    path('<int:proyecto_id>/participante/<int:participante_id>/', views.participante_view,
+         name='participante'),
+    path('<int:proyecto_id>/participantes/', views.participantes_view, name='participantes'),
 
 ]
