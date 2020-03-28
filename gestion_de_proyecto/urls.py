8be22853c1136ebc5dbb501d520d3d451aebd00a
Marcelo <marce-1999@hotmail.com>
2020-03-28 12:02:00 -0400

    Recupera vista de agregacion de nuevo usuario al proyecto.

--- gestion_de_proyecto/urls.py
+++ gestion_de_proyecto/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:proyecto_id>/', views.visualizar_proyecto_view, name='visualizar_proyecto'),
     #path('', views.visualizar_proyectos_view, name='visualizarProyectos'),
     #path('editar/', views.editar_proyecto_view, name='editarProyecto'),
-
+    path('<int:proyecto_id>/participante/nuevo/', views.nuevo_participante_view, name='nuevo_participante'),
+    path('<int:proyecto_id>/permisos_insuficientes', views.pp_insuficientes, name='pp_insuficientes'),
 ]
--- gestion_de_proyecto/views.py
+++ gestion_de_proyecto/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from gestion_de_proyecto.forms import ProyectoForm
+from gestion_de_proyecto.forms import ProyectoForm, NuevoParticipanteForm, SeleccionarPermisosForm
+from roles_de_proyecto.decorators import pp_requerido
+from roles_de_proyecto.models import RolDeProyecto
 from .models import Proyecto
+
 
 # Create your views here.
 
@@ -33,7 +38,6 @@ def nuevo_proyecto_view(request):
     return render(request, 'gestion_de_proyecto/nuevo_proyecto.html', {'formulario': form})
 
 
-
 def visualizar_proyecto_view(request, proyecto_id):
     """
     Vista que muestra al usuario toda la informacion de un proyecto.
@@ -48,5 +52,54 @@ def visualizar_proyecto_view(request, proyecto_id):
         HttpResponse
     """
     proyecto = get_object_or_404(Proyecto, id=proyecto_id)
-    contexto={'user': request.user, 'proyecto': proyecto}
-    return render(request,'gestion_de_proyecto/visualizar_proyecto.html',contexto)
+    contexto = {'user': request.user, 'proyecto': proyecto}
+    return render(request, 'gestion_de_proyecto/visualizar_proyecto.html', contexto)
+
+
+@pp_requerido('pp_agregar_participante')
+def nuevo_participante_view(request, proyecto_id):
+    """
+    Vista que permite la asignacion de un rol
+
+    Args
+
+    request: HttpRequest recibido por el servidor
+    proyecto_id: identificador del proyecto donde se agregara el usuario
+
+    Retorna
+
+    HttpResponse
+    """
+    proyecto = get_object_or_404(Proyecto, id=proyecto_id)
+    contexto = {
+        'user': request.user,
+        'proyecto': proyecto,
+    }
+
+    if len(request.GET.keys()) == 0:
+        form = NuevoParticipanteForm(proyecto=proyecto)
+        contexto['nuevo_participante_form'] = form
+    else:
+        if any(arg not in request.GET.keys() for arg in ['rol', 'usuario']):
+            raise Http404('No se pasaron los argumentos correctos a la URL')
+        if request.method == 'POST':
+            form = NuevoParticipanteForm(request.GET)
+            if form.is_valid():
+                participante = form.save(commit=False)
+                participante.proyecto = proyecto
+                participante.save()
+                permisos_por_fase = {fase[2:]: request.POST[fase] for fase in request.POST.keys() if
+                                     fase.startswith('f_')}
+                participante.asignar_permisos_de_proyecto(permisos_por_fase)
+                #TODO cambiar a donde dirige este redirect
+                return redirect('index')
+        else:
+            rol = RolDeProyecto.objects.get(id=request.GET['rol'])
+            usuario = User.objects.get(id=request.GET['usuario'])
+            contexto['seleccionar_permisos_form'] = SeleccionarPermisosForm(usuario, proyecto, rol)
+
+    return render(request, 'gestion_de_proyecto/nuevo_participante.html', contexto)
+
+
+def pp_insuficientes(request, *args, **kwargs):
+    return render(request, 'gestion_de_proyecto/pp_insuficientes.html', context={'user': request.user})
