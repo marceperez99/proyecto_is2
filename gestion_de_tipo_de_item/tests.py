8339a8a392c38a4ac5f888d9132cfcca79e7bb56
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-29 19:48:55 -0400

    Agrega fixture para atributos
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_tipo_de_item/tests.py
+++ gestion_de_tipo_de_item/tests.py
@@ -1,3 +1,112 @@
-from django.test import TestCase
+from datetime import timezone
+
+import pytest
+from django.contrib.auth.models import User, Permission
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+from gestion_de_fase.models import Fase
+from gestion_de_proyecto.models import Proyecto, Participante
+from gestion_de_tipo_de_item.models import TipoDeItem, AtributoBinario, AtributoCadena, AtributoNumerico, AtributoFecha, \
+    AtributoBooleano
+from roles_de_proyecto.models import RolDeProyecto
+
+
+@pytest.fixture
+def usuario():
+    user = User(username='user_test', email='test@admin.com')
+    user.set_password('password123')
+    user.save()
+    return user
+
+
+@pytest.fixture
+def cliente_loggeado(usuario):
+    client = Client()
+    client.login(username='user_test', password='password123')
+    return client
+
+@pytest.fixture
+def rol_de_proyecto():
+    rol = RolDeProyecto(nombre='Desarrollador', descripcion='Descripcion del rol')
+    rol.save()
+    rol.asignar_permisos(list(Permission.objects.all().filter(codename__startswith='pp_')))
+    return rol
+
+
+@pytest.fixture
+def proyecto(usuario, rol_de_proyecto):
+    proyecto = Proyecto(nombre='Proyecto Prueba', descripcion='Descripcion de prueba', fechaCreacion=datetime.today(),
+                        creador=usuario)
+    proyecto.save()
+    fase = Fase(nombre='Analisis', proyecto=proyecto, faseCerrada=False, puedeCerrarse=False)
+    fase.save()
+    fase = Fase(nombre='Desarrollo', proyecto=proyecto, faseCerrada=False, puedeCerrarse=False)
+    fase.save()
+    fase = Fase(nombre='Pruebas', proyecto=proyecto, faseCerrada=False, puedeCerrarse=False)
+    fase.save()
+    participante = Participante.objects.create(proyecto=proyecto, usuario=usuario)
+    participante.save()
+    return proyecto
+
+@pytest.fixture
+def tipo_de_item(proyecto):
+    tipo_de_item = TipoDeItem()
+    tipo_de_item.nombre = "Requerimiento Funcional"
+    tipo_de_item.descripcion = "Especificación de un requerimiento funcional."
+
+    tipo_de_item.prefijo = "RF"
+    tipo_de_item.creador = usuario
+    tipo_de_item.fase = Fase.objects.get(nombre='Analisis')
+    tipo_de_item.fecha_creacion = timezone.now()
+    tipo_de_item.save()
+    return tipo_de_item
+
+
+
+
+
+@pytest.fixture
+def atributos(tipo_de_item):
+    atributos = []
+    abin = AtributoBinario()
+    abin.requerido = False
+    abin.max_tamaño = 5
+    abin.nombre = "Diagrama del caso de uso"
+    abin.tipo_de_item = tipo_de_item
+    abin.save()
+
+    acad = AtributoCadena()
+    acad.nombre = "Descripción"
+    acad.requerido = True
+    acad.max_longitud = 400
+    acad.tipo_de_item = tipo_de_item
+    acad.save()
+
+    adate = AtributoFecha()
+    adate.nombre = "Fecha de Cierre"
+    adate.requerido = True
+    adate.tipo_de_item = tipo_de_item
+
+    anum = AtributoNumerico()
+    anum.nombre = "Costo del caso de uso"
+    anum.requerido = False
+    anum.max_digitos = 2
+    anum.max_decimales = 2
+    anum.tipo_de_item = tipo_de_item
+    anum.save()
+
+    abool = AtributoBooleano()
+    abool.nombre = "Cierto"
+    abool.requerido = True
+    abool.tipo_de_item = tipo_de_item
+
+    atributos.append(abin)
+    atributos.append(acad)
+    atributos.append(abool)
+    atributos.append(adate)
+    atributos.append(anum)
+    return atributos
+@pytest.mark.django_db
+def test_importar_tipo_de_item(cliente_loggeado,atributos):
