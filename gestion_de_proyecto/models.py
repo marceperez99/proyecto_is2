3c684df28045159eaf5627922dafb0c224cc8e19
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-03-30 20:03:17 -0400

    Migrations de proyecto.
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_proyecto/models.py
+++ gestion_de_proyecto/models.py
@@ -26,8 +26,8 @@ class Proyecto(models.Model):
     """
         Modelo para la clase proyecto
     """
-    nombre = models.CharField(max_length=101)
-    descripcion = models.CharField(max_length=401)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=400)
     gerente = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     creador = models.ForeignKey(User, related_name='proyectos_creador', on_delete=models.CASCADE, null=True)
     fecha_de_creacion = models.DateTimeField(verbose_name="Fecha de Creacion", default=timezone.now)
