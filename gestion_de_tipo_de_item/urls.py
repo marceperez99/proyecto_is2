6ec44bc80fb2bf1d7e5acc365c2fe1cca17815ea
Marcos Riveros <macros21b@fpuna.edu.py>
2020-04-01 20:03:43 -0400

    url de ventana de confirmacion de eliminacion de tipo de item

--- gestion_de_tipo_de_item/urls.py
+++ gestion_de_tipo_de_item/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('tipo_de_item/importar/', views.importar_tipo_de_item_view, name='importar_tipo_de_item'),
     path('tipo_de_item/<int:tipo_de_item_id>/', views.tipo_de_item_view, name='tipo_de_item'),
     path('tipo_de_item/<int:tipo_de_item_id>/editar/', views.editar_tipo_de_item_view, name='editar_tipo_de_item'),
+    path('tipo_de_item/<int:tipo_de_item_id>/eliminar/', views.eliminar_tipo_de_item_view,
+         name='eliminar_tipo_de_item'),
 ]
