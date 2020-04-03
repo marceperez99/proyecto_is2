1c172e12edef7e1887f6d4f9e68d83dbced2801a
Marcelo <marce-1999@hotmail.com>
2020-04-02 20:10:09 -0400

    Cambia el permiso de ver fase.

--- gestion_de_fase/views.py
+++ gestion_de_fase/views.py
@@ -108,7 +108,7 @@ def nueva_fase_view(request, proyecto_id):
 
 @login_required
 @permission_required('roles_de_sistema.pu_acceder_sistema', login_url='sin_permiso')
-@pp_requerido_en_fase('pg_editar_fase')
+@pp_requerido_en_fase('pg_f_editar_fase')
 def editar_fase_view(request, proyecto_id, fase_id):
     """
     Vista que muestra al usuario los datos actuales de la fase que se pueden modificar, si el usuario
