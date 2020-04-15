890acec668ef5d632844a21246e6b5b4158628f8
Marcos Riveros <macros21b@fpuna.edu.py>
2020-04-14 21:00:38 -0400

    path para desasignar rs

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+
 from . import views
+
 urlpatterns = [
     path('usuario/', views.usuarios_view, name='usuarios'),
     path('usuario/<int:usuario_id>/', views.usuario_view, name='perfil_de_usuario'),
     path('usuario/<int:usuario_id>/asignar_rs/', views.usuario_asignar_rol_view, name='asignar_rol_de_sistema'),
+    path('usuario/<int:usuario_id>/desasignar_rs/', views.desasignar_rol_de_sistema_view,
+         name='desasignar_rol_de_sistema'),
     path('panel_de_control/', views.panel_de_administracion_view, name='panel_de_control')
 ]
