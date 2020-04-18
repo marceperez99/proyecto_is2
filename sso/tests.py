ec57713def6892fd6a14220ab7c476da6e8e028b
Marcelo <marce-1999@hotmail.com>
2020-04-17 20:20:10 -0400

    Documenta pruebas unitarias de la app sso

--- sso/tests.py
+++ sso/tests.py
@@ -21,12 +21,31 @@ def rs_admin():
 
 
 def test_index_usuario_no_autenticado():
+    """
+    Prueba unitaria que comprueba que un usuario no logeado sea redirigido a la vista de inicio de sesion.
+
+    Se espera:
+        Que el Status Code de la respuesta del servidor sea HTTPStatus.FOUND.
+
+    Mensaje de Error:
+        El status code de la respuesta del servidor no fue HTTPStatus.FOUND.
+    """
     client = Client()
     response = client.get(reverse('index'))
-    assert response.status_code == HTTPStatus.FOUND
+    assert response.status_code == HTTPStatus.FOUND, 'El status code de la respuesta del servidor no fue ' \
+                                                     'HTTPStatus.FOUND'
 
 @pytest.mark.django_db
 def test_index_usuario_autenticado(rs_admin):
+    """
+    Prueba unitaria que comprueba el funcionaminento correcto de la vista de pantalla de inicio.
+
+    Se espera:
+        Que el Status Code de la respuesta del servidor sea HTTPStatus.OK
+
+    Mensaje de Error:
+        El status code de la respuesta del servidor no fue HTTPStatus.OK
+    """
     # Preparacion de entorno para la prueba
     user = User.objects.create(username='testing')
     user.set_password('12345')
@@ -36,11 +55,20 @@ def test_index_usuario_autenticado(rs_admin):
     client.login(username='testing', password='12345')
     # Prueba de funcionalidad
     response = client.get(reverse('index'))
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.OK, 'El status code de la respuesta del servidor no fue HTTPStatus.OK'
+
 
 @pytest.mark.django_db
 def test_logout():
+    """
+        Prueba unitaria que comprueba el funcionaminento correcto del logout.
 
+        Se espera:
+            Que el Status Code de la respuesta del servidor sea HTTPStatus.FOUND
+
+        Mensaje de Error:
+            El status code de la respuesta del servidor no fue HTTPStatus.FOUND
+        """
     user = User.objects.create(username='testing')
     user.set_password('12345')
     user.save()
@@ -48,4 +76,5 @@ def test_logout():
     client.login(username='testing', password='12345')
 
     response = client.get(reverse('logout'))
-    assert response.status_code == HTTPStatus.FOUND
+    assert response.status_code == HTTPStatus.FOUND, 'El status code de la respuesta del servidor no fue' \
+                                                     ' HTTPStatus.FOUND'
