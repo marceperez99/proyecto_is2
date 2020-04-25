5b5401e27ca6515ecaa17585cc53b44e03ab6a0d
Marcelo <marce-1999@hotmail.com>
2020-04-25 17:31:11 -0400

    Agrega script para crear rol Administrador, script para montar el SSO. Agrega generacion de documentacion y ejecucion de pruebas unitarias antes de reiniciar el servidor Apache

--- scripts/sso_setup.py
+++ scripts/sso_setup.py
@@ -1,12 +1,16 @@
 from allauth.socialaccount.models import SocialApp
 from django.contrib.sites.models import Site
+from django.conf import settings
 
-SECRET_KEY = 'sw2lVjRDgG1GtDAZVC_QmVhr'
-CLIENT_ID = '347886323853-r72mjg1utuabev2f73ngef3tg2nqluth.apps.googleusercontent.com'
-s = Site.objects.get(domain='example.com')
-s.domain = 'localhost:8000'
-s.name = 'localhost'
-s.save()
-sa = SocialApp(provider='google', name='gapi', client_id=CLIENT_ID, secret=SECRET_KEY)
-sa.save()
-sa.sites.set([s])
+SECRET_KEY = settings.GOOGLE_OAUTH_SECRET_KEY
+CLIENT_ID = settings.GOOGLE_OAUTH_CLIENT_ID
+
+
+if SocialApp.objects.filter(name='gapi').exists():
+    s = Site.objects.get(domain='example.com')
+    s.domain = 'localhost:8000'
+    s.name = 'localhost'
+    s.save()
+    sa = SocialApp(provider='google', name='gapi', client_id=CLIENT_ID, secret=SECRET_KEY)
+    sa.save()
+    sa.sites.set([s])
