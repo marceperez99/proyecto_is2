11d02fb0c0b1347c72704b0d61415936991893b4
Marcelo <marce-1999@hotmail.com>
2020-04-26 19:32:35 -0400

    Modifica scripts de montaje de SSO y creacion de rol administrador

--- scripts/sso_setup.py
+++ scripts/sso_setup.py
@@ -5,12 +5,15 @@ from django.conf import settings
 SECRET_KEY = settings.GOOGLE_OAUTH_SECRET_KEY
 CLIENT_ID = settings.GOOGLE_OAUTH_CLIENT_ID
 
-
-if SocialApp.objects.filter(name='gapi').exists():
+print(SocialApp.objects.filter(name='gapi'))
+if not SocialApp.objects.filter(name='gapi').exists():
     s = Site.objects.get(domain='example.com')
     s.domain = 'localhost:8000'
     s.name = 'localhost'
     s.save()
     sa = SocialApp(provider='google', name='gapi', client_id=CLIENT_ID, secret=SECRET_KEY)
     sa.save()
+    print("im in")
     sa.sites.set([s])
+else:
+    print("Algo paso")
