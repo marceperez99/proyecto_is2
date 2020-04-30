f5f6ae3ce0fa7ec4dca16abb256741d05c1fdb1a
Marcelo <marce-1999@hotmail.com>
2020-04-30 17:22:51 -0400

    Cambia permiso necesario para solicitar aprobacion

--- gestion_de_item/views.py
+++ gestion_de_item/views.py
@@ -404,7 +404,7 @@ def relacionar_item_view(request, proyecto_id, fase_id, item_id):
 
 @login_required
 @permission_required('roles_de_sistema.pu_acceder_sistema', login_url='sin_permiso')
-@pp_requerido_en_fase('pp_f_aprobar_item')
+@pp_requerido_en_fase('pp_f_solicitar_aprobacion_item')
 @estado_proyecto(EstadoDeProyecto.INICIADO)
 def solicitar_aprobacion_view(request, proyecto_id, fase_id, item_id):
     """
