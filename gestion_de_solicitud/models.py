45adb2ce287705fd7715e95837ef8dfc72317e6e
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-08-23 17:13:42 -0400

    Agrega modelo de solicitud
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

---
+++ gestion_de_solicitud/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from usuario.models import Usuario
+from gestion_de_item.models import Item
+from gestion_linea_base import LineaBase
+# Create your models here.
+
+class SolicitudDeCambio(models.model):
+    solicitante = models.ForeignKey(Usuario)
+    linea_base = models.ForeignKey(LineaBase)
+    descripcion = models.CharField(max_length=500)
+    fecha = models.DateTimeField()
+    numero_de_miembros = models.IntegerField()
+    votos_a_favor = models.IntegerField(default=0)
+
+class Asignacion(models.model):
+    usuario = models.ForeignKey(Usuario)
+    item = models.ForeignKey(Item)
+    solicitud = models.ForeignKey(SolicitudDeCambio)
--- gestion_linea_base/models.py
+++ gestion_linea_base/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from gestion_de_item.models import Item
 
 class LineaBase(models.Model):
+    #TODO: Marcos, documentar.
     nombre = models.CharField(max_length=50)
     estado = models.CharField(max_length=40)
     items = models.ManyToManyField(Item)
