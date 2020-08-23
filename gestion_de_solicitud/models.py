1b49dee5c0cd8e3bd5931a0f2949688100497511
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-08-23 18:35:33 -0400

    Documenta app solicitud
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_solicitud/models.py
+++ gestion_de_solicitud/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from usuario.models import Usuario
 from gestion_de_item.models import Item
-from gestion_linea_base import LineaBase
+from gestion_linea_base.models import LineaBase
 # Create your models here.
 
 
 class SolicitudDeCambio(models.Model):
-    solicitante = models.ForeignKey(Usuario)
-    linea_base = models.ForeignKey(LineaBase)
+    """
+    Modelo que guarda información de una solicitud de cambio.
+
+
+    """
+    solicitante = models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    linea_base = models.ForeignKey(LineaBase,on_delete=models.CASCADE)
     descripcion = models.CharField(max_length=500)
     fecha = models.DateTimeField()
     numero_de_miembros = models.IntegerField()
@@ -15,11 +20,17 @@ class SolicitudDeCambio(models.Model):
     votos_en_contra = models.IntegerField(default=0)
 
 class Asignacion(models.Model):
-    usuario = models.ForeignKey(Usuario)
-    item = models.ForeignKey(Item)
-    solicitud = models.ForeignKey(SolicitudDeCambio)
+    """
+    Modelo que representa un item y el usuario al que fue asignado dentro de una solicitud de cambio.
+    """
+    usuario = models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+    solicitud = models.ForeignKey(SolicitudDeCambio,on_delete=models.CASCADE)
 
 class Voto(models.Model):
-    miembro = models.ForeignKey(Usuario)
+    """
+    Modelo que maneja el voto de un miembro de de comite
+    """
+    miembro = models.ForeignKey(Usuario,on_delete=models.CASCADE)
     voto_a_favor = models.BooleanField()
-    solicitud = models.ForeignKey(SolicitudDeCambio)
+    solicitud = models.ForeignKey(SolicitudDeCambio,on_delete=models.CASCADE)
