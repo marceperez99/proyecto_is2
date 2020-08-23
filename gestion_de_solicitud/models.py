7d3caf3ecf3ccf305cfa8fd0f1f38d6377663d82
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-08-23 17:42:13 -0400

    Agrega modelo de Voto y Asignacion
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_solicitud/models.py
+++ gestion_de_solicitud/models.py
@@ -4,15 +4,22 @@ from gestion_de_item.models import Item
 from gestion_linea_base import LineaBase
 # Create your models here.
 
-class SolicitudDeCambio(models.model):
+
+class SolicitudDeCambio(models.Model):
     solicitante = models.ForeignKey(Usuario)
     linea_base = models.ForeignKey(LineaBase)
     descripcion = models.CharField(max_length=500)
     fecha = models.DateTimeField()
     numero_de_miembros = models.IntegerField()
     votos_a_favor = models.IntegerField(default=0)
+    votos_en_contra = models.IntegerField(default=0)
 
-class Asignacion(models.model):
+class Asignacion(models.Model):
     usuario = models.ForeignKey(Usuario)
     item = models.ForeignKey(Item)
+    solicitud = models.ForeignKey(SolicitudDeCambio)
+
+class Voto(models.Model):
+    miembro = models.ForeignKey(Usuario)
+    voto_a_favor = models.BooleanField()
     solicitud = models.ForeignKey(SolicitudDeCambio)
