22c8845740f32986426e24959d62d115b3265c8f
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-08-24 11:42:23 -0400

    arregla version
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_solicitud/models.py
+++ gestion_de_solicitud/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from usuario.models import Usuario
 from gestion_de_item.models import Item
-from gestion_linea_base import LineaBase
+from gestion_linea_base.models import LineaBase
 # Create your models here.
 
-class SolicitudDeCambio(models.model):
+class SolicitudDeCambio(models.odel):
     solicitante = models.ForeignKey(Usuario)
     linea_base = models.ForeignKey(LineaBase)
     descripcion = models.CharField(max_length=500)
