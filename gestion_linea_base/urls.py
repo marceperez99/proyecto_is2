aef3aec2df94c76763743296a23bbdf1b4d9b770
Marcos Riveros <macros21b@fpuna.edu.py>
2020-08-26 04:31:37 -0400

    url de lista de lb

--- gestion_linea_base/urls.py
+++ gestion_linea_base/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.listar_linea_base_view, name='listar_linea_base'),
     path('nuevo/', views.nueva_linea_base_view, name='nuevo_linea_base'),
 ]
