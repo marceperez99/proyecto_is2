f5217235e0e8778ac9a9d336a97622a81036aec0
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-08-27 10:58:32 -0400

    deleted migration
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_item/mocks.py
+++
@@ -1,7 +0,0 @@
-class LineaBase():
-    def __init__(self):
-        self.comprometida = False
-    def set_comprometida(self):
-        self.comprometida = True
-    def unset_comprometida(self):
-        self.comprometida = False
