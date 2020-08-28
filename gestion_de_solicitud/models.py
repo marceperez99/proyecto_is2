311179e00e9942f3270abffa56beff8505e4b2fb
Hugo Fleitas <hugofleitasbus@gmail.com>
2020-08-28 11:00:59 -0400

    Agrega comentario
    Signed-off-by: Hugo Fleitas <hugofleitasbus@gmail.com>

--- gestion_de_solicitud/models.py
+++ gestion_de_solicitud/models.py
@@ -41,7 +41,7 @@ class Asignacion(models.Model):
     usuario = models.ForeignKey(Participante, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     solicitud = models.ForeignKey(SolicitudDeCambio, on_delete=models.CASCADE)
-
+    comentario = models.CharField(max_length=500)
 
 class Voto(models.Model):
     """
