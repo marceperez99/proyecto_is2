9fe1fe119b84bc7aab4b666d65a17b7f7c407663
lapm <luisperez.lpr91@fpuna.edu.py>
2020-08-27 21:58:20 -0400

    Agrega vista de listar_solicitudes, solicitud_view, solicitud_votacion,

--- gestion_de_solicitud/views.py
+++ gestion_de_solicitud/views.py
@@ -1,5 +1,78 @@
+from django.contrib.auth.decorators import permission_required, login_required
 from django.forms import inlineformset_factory
 from django.shortcuts import render
+from django.urls import reverse
+from gestion_de_solicitud.models import SolicitudDeCambio, Voto
+from django.shortcuts import get_object_or_404, redirect
+from gestion_de_proyecto.decorators import estado_proyecto
+from gestion_de_proyecto.models import Proyecto, EstadoDeProyecto
 
-# Create your views here.
+@login_required
+@permission_required('roles_de_sistema.pu_acceder_sistema', login_url='sin_permiso')
+@estado_proyecto(EstadoDeProyecto.INICIADO)
+def listar_solicitudes_view(request, proyecto_id):
+    """
+        Vista que permite la visualizacion de las solicitudes creados dentro de un proyecto.
+    """
+    solicitudes = SolicitudDeCambio.objects.filter(linea_base__fase__proyecto_id=proyecto_id).all()
+    proyecto = get_object_or_404(Proyecto, id=proyecto_id)
+    contexto = {
+        'solicitudes': solicitudes,
+        'breadcrumb': {
+            'pagina_actual': 'Fases',
+            'links': [{'nombre': proyecto.nombre, 'url': reverse('visualizar_proyecto', args=(proyecto_id,))}]
+        }
+    }
+    return render(request, 'gestion_de_solicitud/listar_solicitudes.html', contexto)
 
+
+@login_required
+@permission_required('roles_de_sistema.pu_acceder_sistema', login_url='sin_permiso')
+@estado_proyecto(EstadoDeProyecto.INICIADO)
+def solicitud_view(request, proyecto_id, solicitud_id):
+    """
+        Vista que permite la visualizacion de una solicitud en particular.
+    """
+    solicitudes = SolicitudDeCambio.objects.filter(linea_base__fase__proyecto_id=proyecto_id).all()
+    proyecto = get_object_or_404(Proyecto, id=proyecto_id)
+    contexto = {
+        'solicitudes': solicitudes,
+        'breadcrumb': {
+            'pagina_actual': 'Fases',
+            'links': [{'nombre': proyecto.nombre, 'url': reverse('visualizar_proyecto', args=(proyecto_id,))}]
+        }
+    }
+    return render(request, 'gestion_de_solicitud/listar_solicitudes.html', contexto)
+
+
+@login_required
+@permission_required('roles_de_sistema.pu_acceder_sistema', login_url='sin_permiso')
+@estado_proyecto(EstadoDeProyecto.INICIADO)
+def solicitud_votacion_view(request, proyecto_id, solicitud_id):
+    """
+    :param request:
+    :param proyecto_id:
+    :param solicitud_id:
+    :return:
+    """
+    proyecto = get_object_or_404(Proyecto, id=proyecto_id)
+    solicitud = get_object_or_404(SolicitudDeCambio, id=solicitud_id)
+    participante = proyecto.get_participante(request.user)
+    es_participante = proyecto.get_comite_de_cambios().es_miembro(participante)
+    if not es_participante:
+        return redirect('visualizar_proyecto', proyecto.id)
+
+    if solicitud.ya_voto(participante):
+        return redirect('solicitud_de_cambio', proyecto.id)
+
+    if 'voto' in request.GET.keys():
+        voto = Voto(miembro=participante, solicitud_id=solicitud_id)
+        if request.GET['voto'] == 'a_favor' or request.GET['voto'] == 'en_contra':
+            voto.voto_a_favor = request.GET['voto'] == 'a_favor'
+            voto.save()
+            return redirect('solicitud_de_cambio', proyecto.id)
+
+    contexto = {'proyecto': proyecto,
+                'solicitud': solicitud,
+                }
+    return render(request, 'gestion_de_solicitud/votar_solicitud.html', contexto)
