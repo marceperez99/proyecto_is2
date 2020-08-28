238a10dc0a047c8fbc43880d6b799cffa434a5d5
lapm <luisperez.lpr91@fpuna.edu.py>
2020-08-27 21:58:49 -0400

    Agrega urls para las vistas

--- gestion_de_solicitud/urls.py
+++ gestion_de_solicitud/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpattern = [
     path('rompimiento/', views.solicitar_rompimiento_view, name='solicitar_rompimiento'),
+    path('solicitud/', views.listar_solicitudes_view, name='solicitudes_de_cambio'),
+    path('solicitud/<int:solicitud_id>/', views.solicitud_view, name='solicitud_de_cambio'),
+    path('solicitud/<int:solicitud_id>/votar/', views.solicitud_votacion_view, name='votar_solicitud'),
+
 ]
