ae6fe1f7562f391606051bb7f42453609fa0e215
Marcos Riveros <macros21b@fpuna.edu.py>
2020-08-31 00:43:33 -0400

    agreagar url de ver linea de base

--- gestion_linea_base/urls.py
+++ gestion_linea_base/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('nuevo/', views.nueva_linea_base_view, name='nuevo_linea_base'),
+    path('<int:linea_base_id>/', views.visualizar_linea_base_view, name='visualizar_linea_base'),
     path('', views.listar_linea_base_view, name='listar_linea_base'),
     path('<int:linea_base_id>/solicitar_rompimiento/', views.solicitar_rompimiento_view, name='solicitar_rompimiento'),
 ]
