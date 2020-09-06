596fc94db09d2e49ab8df4925505b981a9273e2f
Marcelo <marce-1999@hotmail.com>
2020-09-06 19:34:01 -0400

    Modifica script de instalacion en entorno de desarrollo

--- scripts/build_development.sh
+++ scripts/build_development.sh
@@ -22,6 +22,30 @@ DB_USER="proyecto_user_test"
 DB_PASS="Pr0yect0Test"
 GIT_URL="https://github.com/marzeperez99/proyecto_is2.git"
 TAG="iteracion_3"
+GDRIVE_JSON_PATH="proyecto_is2/proyecto_is2/settings/credenciales/gdriveaccess.json"
+
+#Lectura de los datos de la Base de datos
+read -p "Ingrese el usuario de PostgreSQL [$POSTGRES_USER]: " input
+POSTGRES_USER=${input:-$POSTGRES_USER}
+read -p "Ingrese la contraseña del usuario de PostgreSQL [$POSTGRES_PASS]: " input
+POSTGRES_PASS=${input:-$POSTGRES_PASS}
+read -rp "Ingrese el SECRET KEY del servicio de Google OAuth [$GOOGLE_OAUTH_SECRET_KEY]: " input
+GOOGLE_OAUTH_SECRET_KEY=${input:-$GOOGLE_OAUTH_SECRET_KEY}
+GOOGLE_OAUTH_CLIENT_ID="628176483267-fu449k587f887bm7n5tgc8alndtb35t1.apps.googleusercontent.com"
+read -rp "Ingrese el CLIENT ID del servicio de Google OAuth [$GOOGLE_OAUTH_CLIENT_ID]: " input
+GOOGLE_OAUTH_CLIENT_ID=${input:-$GOOGLE_OAUTH_CLIENT_ID}
+
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE=gdriveaccess.json
+read -rp "Ingrese la ruta del archivo el contenido de las credenciales proveidas para el uso de la plataforma de Google Drive [$GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE]: " input
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE=${input:-$GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE}
+echo "$GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE"
+if [ ! -f "$GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE" ]; then
+  echo "Archivo de credenciales no existente"
+  exit 1
+fi
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE=$(cat "$GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE")
+
+
 
 #Obtencion del codigo del repositorio remoto
 git clone $GIT_URL
@@ -31,13 +55,22 @@ TAG=${input:-$TAG}
 # Se accede al tag correspondiente
 git checkout tags/"$TAG" -b "$TAG"
 
-#Lectura de los datos de la Base de datos
-read -p "Ingrese el usuario de PostgreSQL [$POSTGRES_USER]: " input
-POSTGRES_USER=${input:-$POSTGRES_USER}
-read -p "Ingrese la contraseña del usuario de PostgreSQL [$POSTGRES_PASS]: " input
-POSTGRES_PASS=${input:-$POSTGRES_PASS}
+# Se guarda las credenciales de Google Drive
+echo "$GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE" | sudo tee "$GDRIVE_JSON_PATH" > /dev/null;
 
 #Creacion de nueva base de datos
 scripts/build_database.sh "$DB_NAME" "$POSTGRES_USER" "$POSTGRES_PASS" "$DB_USER" "$DB_PASS"
-scripts/run_dev_server.sh
 
+export DJANGO_SETTINGS_MODULE=proyecto_is2.settings.dev_settings
+pipenv run pipenv install
+pipenv run python manage.py migrate
+
+# Carga de Datos
+TEMP_DIR=$(mktemp -d)
+SSO_KEYS="$TEMP_DIR/google_keys.json"
+scripts/data/sso_config.sh "$GOOGLE_OAUTH_CLIENT_ID" "$GOOGLE_OAUTH_SECRET_KEY" > "$SSO_KEYS"
+pipenv run python manage.py loaddata "$SSO_KEYS"
+pipenv run python manage.py shell < "scripts/create_admin.py"
+#pipenv run python manage.py loaddata data.json
+
+scripts/run_server.sh -d
--- scripts/build_production.sh
+++ scripts/build_production.sh
@@ -134,10 +134,20 @@ echo "- Dependencias instaladas"
 python manage.py migrate
 echo "- Migraciones cargadas"
 # Se cargan datos
+TEMP_DIR=$(mktemp -d)
+SSO_KEYS="$TEMP_DIR/google_keys.json"
 
-scripts/load_data.sh  "$GOOGLE_OAUTH_CLIENT_ID" "$GOOGLE_OAUTH_SECRET_KEY"
+scripts/data/sso_config.sh "$GOOGLE_OAUTH_CLIENT_ID" "$GOOGLE_OAUTH_SECRET_KEY" > "$SSO_KEYS"
+# Se cargan los datos del OAUTH
+cat "$SSO_KEYS"
+python manage.py loaddata "$SSO_KEYS"
+rm "$SSO_KEYS"
+# Se crea el super usuario
+python manage.py shell < "scripts/create_admin.py"
+
+# TODO Se carga datos de prueba
+# python manage.py loaddata scripts/data/data.json
 
 cd scripts || exit 1
 # Se corre el servidor
-export VENV_PATH="/var/www/proyecto_is2/venv/bin/activate"
 ./run_server.sh -p;
