c1840a6267832a1bb19e7b0f2af82aaabffd27df
Marcos Riveros <macros21b@fpuna.edu.py>
2020-09-16 20:37:25 -0400

    metodo de creacion de nombre de LB

---
+++ gestion_linea_base/utils.py
@@ -0,0 +1,19 @@
+from gestion_linea_base.models import LineaBase
+
+
+def create_nombre_LB(proyecto, fase):
+    # TODO: Marcos, poner en planilla.
+    """
+    Función utilitaria que construye el nombre para la Línea de Base de tal forma que estos no se repitan,
+    el patron de nonbre será "LB_<nro_fase>_<nro_lb>"
+
+    Argumentos:
+        - proyecto: Proyectos
+        - fase: Fase
+
+    Retorna
+        El nombre único para la linea de Base a crear.
+   """
+    nro_fase = proyecto.get_fases().index(fase) + 1
+    nro_lb = LineaBase.objects.filter(fase=fase).__len__() + 1
+    return "LB_" + str(nro_fase) + "_" + str(nro_lb)
