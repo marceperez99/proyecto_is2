7b03bb7e19588ad16896342f8456ffdcf083566f
Marcos Riveros <macros21b@gmail.com>
2020-10-10 20:24:20 -0300

    documentacion funcion de enviar mail de nuevo usuario

--- usuario/tasks.py
+++ usuario/tasks.py
@@ -8,6 +8,16 @@ from .models import Usuario
 
 @shared_task
 def mail_for_new_user(id_user):
+    """
+    Funcion utilitaria que se encarga de enviar un mail a los Administradores del sistema,
+    informando que un usuario a sido creado y necesita un Rol.
+
+    Argumentos:
+        - id_user: Id del usuario creado
+
+    Retorna:
+        - Void
+    """
     user = User.objects.get(id=id_user)
     admins = User.objects.filter(groups__name='Administrador')
     mail_admins = [a.email.__str__() for a in admins]
