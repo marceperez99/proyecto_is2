df21a7cc8da4e88f0804e1a043ef0980538c6752
Marcelo <marce-1999@hotmail.com>
2020-10-10 13:31:41 -0300

    Modifica scripts de produccion para descargar un release en vez de descargar el repositorio

--- scripts/data/apache_config.sh
+++ scripts/data/apache_config.sh
@@ -1,24 +1,32 @@
 BASE_DIR=$1;
 echo "
       <VirtualHost *:80>
-        ServerAdmin webmaster@localhost
-        DocumentRoot $BASE_DIR/html
+	ServerAdmin webmaster@localhost
+	DocumentRoot /var/www/proyecto_is2
 
-        ErrorLog $BASE_DIR/proyecto_is2/site/logs/error.log
-        CustomLog $BASE_DIR/proyecto_is2/site/logs/access.log combined
+	ErrorLog /var/www/proyecto_is2/site/logs/error.log
+	CustomLog /var/www/proyecto_is2/site/logs/access.log combined
 
-        alias /static $BASE_DIR/proyecto_is2/site/public/static
-        <Directory $BASE_DIR/proyecto_is2/site/public/static>
-          Require all granted
-        </Directory>
+	alias /static /var/www/proyecto_is2/site/public/static
 
-        <Directory $BASE_DIR/proyecto_is2/django/proyecto_is2/proyecto_is2>
-          <Files wsgi.py>
-            Require all granted
-          </Files>
-        </Directory>
-        WSGIDaemonProcess proyecto_is2 python-path=$BASE_DIR/proyecto_is2/django/proyecto_is2/ python-home=$BASE_DIR/proyecto_is2/venv
-        WSGIProcessGroup proyecto_is2
-        WSGIScriptAlias / $BASE_DIR/proyecto_is2/django/proyecto_is2/proyecto_is2/wsgi.py
-      </VirtualHost>
-    "
+	<Directory /var/www/proyecto_is2>
+		Require host localhost
+  		Require ip 127.0.0.1
+  		Require ip 192.168
+  		Require ip 10
+	</Directory>
+
+	<Directory /var/www/proyecto_is2/site/public/static>
+		Require all granted
+	</Directory>
+
+	<Directory /var/www/proyecto_is2/django/proyecto_is2/proyecto_is2>
+		<Files wsgi.py>
+			Require all granted
+		</Files>
+	</Directory>
+	WSGIDaemonProcess proyecto_is2 python-path=/var/www/proyecto_is2/django/proyecto_is2/ python-home=/var/www/proyecto_is2/venv
+	WSGIProcessGroup proyecto_is2
+	WSGIScriptAlias / /var/www/proyecto_is2/django/proyecto_is2/proyecto_is2/wsgi.py
+</VirtualHost>
+"
