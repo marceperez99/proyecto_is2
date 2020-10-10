9811161c0e6be578f23ba3106f71214812fced5f
Marcos Riveros <macros21b@gmail.com>
2020-10-10 20:30:53 -0300

    documentacion de signal de creacion de usuario

--- usuario/signals.py
+++ usuario/signals.py
@@ -7,6 +7,18 @@ from .models import Usuario
 
 @receiver(post_save, sender=User)
 def save_user(sender, instance, created, **kwargs):
+    """
+    Funcion utilitaria que se encarga de llamar a la funcion que envia un mail a los Administradores del sistema, cada
+    vez que un usuario es creado.
+
+    Argumentos:
+        - sender: Clase del Usuario.
+        - instance: Instancia del Usuario.
+        - created: Bandera que identifica si la instancia del usuario fue creada o editada.
+
+    Retorna:
+        - Void
+    """
     if created:
         print('User created')
         mail_for_new_user.delay(instance.id)
