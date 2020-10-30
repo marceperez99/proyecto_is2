37a69453b08f6daf52c9ecc440abb56cba9f8d45
jbus97 <hugofleitasbus@gmail.com>
2020-10-30 10:38:07 -0300

    Agrega vista de reporte de linea base

--- gestion_linea_base/urls.py
+++ gestion_linea_base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:linea_base_id>/', views.visualizar_linea_base_view, name='visualizar_linea_base'),
     path('', views.listar_linea_base_view, name='listar_linea_base'),
     path('<int:linea_base_id>/solicitar_rompimiento/', views.solicitar_rompimiento_view, name='solicitar_rompimiento'),
+    path('<int:linea_base_id>/reporte/', views.reporte_linea_base_view, name='reporte_linea_base'),
 ]
