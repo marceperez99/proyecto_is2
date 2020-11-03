3ee01ecdd4214ce32d3f8aee56bc0091bc8a6254
lapm95 <luisperez.lpr91@fpuna.edu.py>
2020-11-03 20:59:13 -0300

    path para la vista reporte de proyecto

--- gestion_de_proyecto/urls.py
+++ gestion_de_proyecto/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('nuevo/', views.nuevo_proyecto_view, name='nuevo_proyecto'),
     path('<int:proyecto_id>/', views.visualizar_proyecto_view, name='visualizar_proyecto'),
-    path('<int:proyecto_id>/info/', views.info_proyecto_view,name='info_proyecto'),
+    path('<int:proyecto_id>/info/', views.info_proyecto_view, name='info_proyecto'),
     path('<int:proyecto_id>/editar/', views.editar_proyecto_view, name='editar_proyecto'),
     path('<int:proyecto_id>/cancelar/', views.cancelar_proyecto_view, name='cancelar_proyecto'),
     path('<int:proyecto_id>/iniciar/', views.iniciar_proyecto_view, name='iniciar_proyecto'),
@@ -18,5 +18,6 @@ urlpatterns = [
     path('<int:proyecto_id>/participante/', views.participantes_view, name='participantes'),
     path('<int:proyecto_id>/participante/<int:participante_id>/eliminar/', views.eliminar_participante_view,
          name='eliminar_participante'),
-    path('<int:proyecto_id>/comite/asignar',views.seleccionar_miembros_del_comite_view,name='asignar_miembros_comite')
+    path('<int:proyecto_id>/comite/asignar',views.seleccionar_miembros_del_comite_view, name='asignar_miembros_comite'),
+    path('<int:proyecto_id>/reporte/', views.reporte_de_proyecto_view, name='reporte_de_proyecto'),
 ]
